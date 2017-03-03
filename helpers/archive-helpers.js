4159d5c361846f7f46b9700a48cee3077befe773
dbrodie122 <david.brodie122@gmail.com>
2017-03-02 19:09:07 -0800

    Pass 8/9 tests.  Work for download URL is not done.

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -66,4 +67,19 @@ exports.isUrlArchived = function(url, callback) { //both?
 };
 
 exports.downloadUrls = function(urls) { //worker?
+  //REMEMBER!
+    //WE WERE TRYING TO FIGURE OUT HOW TO USE REQUEST AND I COULDNT GET IT TO CONSOLE LOG ANYTHING
+  //console.log('+++++++++++++urls +++++++: ', url.parse(urls[0]));
+  // urls.forEach(url => {
+  //   console.log("=======using request.get======", request.get(url));
+  // });
 };
+
+// request('http://www.google.com', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+//request module
+//readfile from external site

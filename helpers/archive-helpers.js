0d31509262aef199d69af2110188bc4c1313b9eb
dbrodie122 <david.brodie122@gmail.com>
2017-03-02 20:49:05 -0800

    Pass all tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 var request = require('request');
+var url = require('url');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -67,19 +68,9 @@ exports.isUrlArchived = function(url, callback) { //both?
 };
 
 exports.downloadUrls = function(urls) { //worker?
-  //REMEMBER!
-    //WE WERE TRYING TO FIGURE OUT HOW TO USE REQUEST AND I COULDNT GET IT TO CONSOLE LOG ANYTHING
-  //console.log('+++++++++++++urls +++++++: ', url.parse(urls[0]));
-  // urls.forEach(url => {
-  //   console.log("=======using request.get======", request.get(url));
-  // });
+
+  urls.forEach(url => {
+    request('http://' + url).pipe(fs.createWriteStream(exports.paths.archivedSites + '/' + url));  
+  });
 };
 
-// request('http://www.google.com', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-//request module
-//readfile from external site
